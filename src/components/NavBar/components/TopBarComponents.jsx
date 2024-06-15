0969a9664ef576fceed5003bc2aa003acfa02c81
Carlos <thepshbv@gmail.com>
2024-06-15 23:04:03 +0200

    ara zi

--- src/components/NavBar/components/TopBarComponents.jsx
+++ src/components/NavBar/components/TopBarComponents.jsx
@@ -1,53 +1,60 @@
 import { NavLink } from 'react-router-dom';
-import { Link } from 'react-router-dom';
 import './TopBar.css';
 import logo from '../../../assets/prolog_logo.svg';
 import React, { useState } from 'react';
 
-export const TopBarComponents = () => {
-
-  const [loggedIn, setLoggedIn] = useState(false);
-
-  const handleLogout = () => {
-    setLoggedIn(false);
-    window.location.reload();
-  };
-
+export const TopBarComponents = ({ setLoggedInCallback, loggedIn }) => {
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
 
- 
   const handleLogin = () => {
     const storedUsers = localStorage.getItem('users');
     const usersData = storedUsers ? JSON.parse(storedUsers) : {};
     
-    if (usersData[username] === password) {
-      setLoggedIn(true);
+    if (usersData[username] && usersData[username] === password) {
+      setLoggedInCallback(true);
     } else {
       alert('Usuario o contraseña incorrecto');
     }
   };
 
+  const handleLogout = () => {
+    setLoggedInCallback(false);
+    window.location.reload();
+    window.location.href = '/';
+  };
 
   return (
     <div className="top-nav">
       <h2 className="logoImage">
-          <NavLink to="/introduccion" style={{ paddingBottom: '0.313rem' }}>
-            <img src={logo} alt="Logo Icon" />
-          </NavLink>
+        <NavLink to="/introduccion" style={{ paddingBottom: '0.313rem' }}>
+          <img src={logo} alt="Logo Icon" />
+        </NavLink>
       </h2>
       {loggedIn ? (
         <div className="user-info">
-          <p class="welcome">{username}</p>
-          <button class="logout" onClick={handleLogout}>Cerrar sesión</button>
+          <p className="welcome">{username}</p>
+          <button className="logout" onClick={handleLogout}>Cerrar sesión</button>
         </div>
       ) : (
-        <div class="buttons-container">
-          <input class="usuario" type="username" placeholder="Usuario" value={username} onChange={(e) => setUsername(e.target.value)} />
-          <input class="contraseña" type="password" placeholder="Contraseña" value={password} onChange={(e) => setPassword(e.target.value)} />
-          <button class="login-button" onClick={handleLogin}>Iniciar Sesión</button>
+        <div className="buttons-container">
+          <input
+            className="usuario"
+            type="text"
+            placeholder="Usuario"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+          />
+          <input
+            className="contraseña"
+            type="password"
+            placeholder="Contraseña"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <button className="login-button" onClick={handleLogin}>Iniciar Sesión</button>
           <NavLink to="/register">
-            <button class="register-button">Registrarse</button>
+            <button className="register-button">Registrarse</button>
           </NavLink>
         </div>
       )}
--- src/components/NavBar/containers/NavBar.jsx
+++ src/components/NavBar/containers/NavBar.jsx
@@ -1,17 +1,16 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { NavBarComponents } from '../components/NavBarComponents';
 import { TopBarComponents } from '../components/TopBarComponents';
 
 export const NavBar = (props) => {
   const { children } = props;
+  const [loggedIn, setLoggedIn] = useState(false);
+
   return (
-    <div
-      className="organizer"
-      style={{ background: 'linear-gradient(to right, #fcfcfc, #dcdcdc)' }}
-    >
-      <TopBarComponents />
+    <div className="organizer" style={{ background: 'linear-gradient(to right, #fcfcfc, #dcdcdc)' }}>
+      <TopBarComponents setLoggedInCallback={setLoggedIn} loggedIn={loggedIn} />
       <div style={{ display: 'flex', flexDirection: 'row', height: '100%' }}>
-        <NavBarComponents />
+        {loggedIn && <NavBarComponents />}
         <div style={{ width: '100%', marginLeft: '10px' }}>{children}</div>
       </div>
     </div>
@@ -19,4 +18,3 @@ export const NavBar = (props) => {
 };
 
 export default NavBar;
-
