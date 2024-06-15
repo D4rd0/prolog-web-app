e1bd3ee825bf1b84bd30785ff54b3f9f5a50acbc
Carlos <thepshbv@gmail.com>
2024-06-15 21:47:57 +0200

    f5

--- src/components/NavBar/components/TopBarComponents.jsx
+++ src/components/NavBar/components/TopBarComponents.jsx
@@ -10,29 +10,7 @@ export const TopBarComponents = () => {
 
   const handleLogout = () => {
     setLoggedIn(false);
-    window.isCorrectReglasEjercicio = 1;
-    window.isCorrectReglasCuestionario = 1;
-
-    window.isCorrectHechosEjercicio = 1;
-    window.isCorrectHechosCuestionario = 1;
-
-    window.isCorrectTerminosEjercicio = 1;
-    window.isCorrectTerminosCuestionario = 1;
-
-    window.isCorrectClausulasEjercicio = 1;
-    window.isCorrectClausulasCuestionario = 1;
-
-    window.isCorrectConsultasEjercicio = 1;
-    window.isCorrectConsultasCuestionario = 1;
-
-    window.isCorrectNegacionEjercicio = 1;
-    window.isCorrectNegacionCuestionario = 1;
-
-    window.isCorrectListasEjercicio = 1;
-    window.isCorrectListasCuestionario = 1;
-
-    window.isCorrectOperadoresEjercicio = 1;
-    window.isCorrectOperadoresCuestionario = 1;
+    window.location.reload();
   };
 
   const [username, setUsername] = useState('');
